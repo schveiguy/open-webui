121f7a866f0830f29a18d371bca4c180b483e928
Steven Schveighoffer <schveiguy@gmail.com>
2025-09-11 22:00:34 -0400

    Update migrations to insert mcp tables in the timeline middle instead of at the end.

--- backend/open_webui/migrations/versions/018012973d35_add_indexes.py
+++ backend/open_webui/migrations/versions/018012973d35_add_indexes.py
@@ -1,7 +1,7 @@
 """Add indexes
 
 Revision ID: 018012973d35
-Revises: d31026856c01
+Revises: 5b1a2c3d4e5f
 Create Date: 2025-08-13 03:00:00.000000
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "018012973d35"
-down_revision = "d31026856c01"
+down_revision = "5b1a2c3d4e5f"
 branch_labels = None
 depends_on = None
 
